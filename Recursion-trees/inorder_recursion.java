7bc96058bac5315c22ba612d8bf92ed9dfacffa1
animesh pandey <ompandey463@gmail.com>
2023-09-15 21:06:03 +0530

    sep15

---
+++ Recursion-trees/inorder_recursion.java
@@ -0,0 +1,33 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+
+    //Recursion method-->
+    // public static void intraverser(TreeNode node,List<Integer> somelist){
+    //     if(node==null){
+    //         return;
+    //     }
+    //     intraverser(node.left,somelist);
+    //     somelist.add(node.val);
+    //     intraverser(node.right,somelist);
+    // }
+
+    // public List<Integer> inorderTraversal(TreeNode root) {
+    //     List<Integer> answer = new ArrayList<>();
+    //     intraverser(root,answer);
+    //     return answer;
+    // }
+}
